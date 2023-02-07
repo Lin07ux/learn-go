3bd836c1bd5d05032ff7c401f5d3c41b3a92b582
Lin07ux <linshengli.linux7@icloud.com>
2023-02-07 15:51:48 +0800

    feat(v1): 响应向左、向右和空格按键

--- game-ebitengine/main.go
+++ game-ebitengine/main.go
@@ -1,29 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"log"
 )
 
-type Game struct{}
+type Game struct {
+	input *Input
+}
+
+func NewGame() *Game {
+	return &Game{
+		input: &Input{"Hello, World"},
+	}
+}
 
 func (g *Game) Update() error {
+	g.input.Update()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World")
+	ebitenutil.DebugPrint(screen, g.input.msg)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
 	return 320, 240
 }
 
+type Input struct {
+	msg string
+}
+
+func (i *Input) Update() {
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		fmt.Println("←←←←←←←←←←←←←←←←←←←←←←←")
+		i.msg = "left pressed"
+	} else if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		fmt.Println("→→→→→→→→→→→→→→→→→→→→→→→")
+		i.msg = "right pressed"
+	} else if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		fmt.Println("-----------------------")
+		i.msg = "space pressed"
+	}
+}
+
 func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("外星人入侵")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
