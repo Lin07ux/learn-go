120fa5df3379e46fa695d3764c17f6819e6a376f
Lin07ux <linshengli.linux7@icloud.com>
2023-02-15 13:05:49 +0800

    gin: add GET/POST/PUT/DELETE json api

--- web-gin/main.go
+++ web-gin/main.go
@@ -1,12 +1,35 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSONP(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "GET",
+		})
+	})
+	r.POST("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "POST",
+		})
+	})
+	r.PUT("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "PUT",
+		})
+	})
+	r.DELETE("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "DELETE",
 		})
 	})
 	_ = r.Run()
