be5dfd12217f436aafbf661c1c673f0cbf2986ad
Lin07ux <linshengli.linux7@icloud.com>
2023-02-15 13:33:49 +0800

    gin: add html template route

--- web-gin/main.go
+++ web-gin/main.go
@@ -7,9 +7,20 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	r.LoadHTMLGlob("templates/**/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index/index.tmpl", gin.H{
+			"title": "Main Website",
+		})
+	})
+	r.GET("/posts", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "posts/index.tmpl", gin.H{
+			"title": "Posts",
+		})
+	})
+	r.GET("/users", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users/index.tmpl", gin.H{
+			"title": "Users",
 		})
 	})
 	r.GET("/books", func(c *gin.Context) {
