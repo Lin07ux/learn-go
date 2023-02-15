d70090d08eb69c780fbbde6ee9344c0d8f63c7ec
Lin07ux <linshengli.linux7@icloud.com>
2023-02-15 13:40:06 +0800

    gin: custom template funcs

--- web-gin/main.go
+++ web-gin/main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"net/http"
+	"time"
 )
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
+}
 
 func main() {
 	r := gin.Default()
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
 	r.LoadHTMLGlob("templates/**/*")
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index/index.tmpl", gin.H{
 			"title": "Main Website",
+			"date":  time.Date(2023, 2, 14, 15, 13, 14, 0, time.Local),
 		})
 	})
 	r.GET("/posts", func(c *gin.Context) {
