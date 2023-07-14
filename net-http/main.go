cc52cf49efb05b0b65935e780b3c8636d984e9c6
林胜利 <linshengli.linux7@icloud.com>
2023-07-14 14:16:48 +0800

    net-http: Hello World with struct

--- net-http/main.go
+++ net-http/main.go
@@ -6,16 +6,20 @@ import (
 	"net/http"
 )
 
+type HelloWorld struct {
+	content string
+}
+
+func (h *HelloWorld) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, h.content)
+}
+
 func main() {
-	http.HandleFunc("/", HelloHandler)
+	http.Handle("/", &HelloWorld{content: "Hello World Struct"})
 
 	log.Println("Http server listening on: http://localhost:8800")
 	err := http.ListenAndServe(":8800", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	_, _ = fmt.Fprintf(w, "Hello World")
 }
