7e5f5923164a52010fc42af3bdd8a80b67a72820
林胜利 <linshengli.linux7@icloud.com>
2023-07-14 16:12:04 +0800

    net-http: start server with signal

--- net-http/main.go
+++ net-http/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 type HelloWorld struct {
@@ -15,11 +19,30 @@ func (h *HelloWorld) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.Handle("/", &HelloWorld{content: "Hello World Struct"})
+	mux := http.NewServeMux()
+	mux.Handle("/", &HelloWorld{content: "Hello World Struct"})
 
-	log.Println("Http server listening on: http://localhost:8800")
-	err := http.ListenAndServe(":8800", nil)
-	if err != nil {
-		log.Fatal(err)
+	server := http.Server{
+		Addr:    ":8800",
+		Handler: mux,
+	}
+
+	// 创建系统信号接收器
+	done := make(chan os.Signal)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-done
+		if err := server.Shutdown(context.Background()); err != nil {
+			log.Fatal("Shutdown server failed:", err)
+		}
+	}()
+
+	log.Println("Starting Http server on: http://localhost" + server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		if err == http.ErrServerClosed {
+			log.Println("Server closed under request")
+		} else {
+			log.Fatal(err)
+		}
 	}
 }
